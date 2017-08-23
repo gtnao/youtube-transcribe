bdacfbf612e8f88867f8c498a50c2ad6724d8708
naotaka nakane <n.nakane@nakane-mbp.local>
2017-08-23 22:35:43 +0900

    update

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,58 +1,67 @@
 'use strict';
 
 (function() {
+  console.log('contents script is loading'); // debug
+
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
   // constructor
   var Content = function() {
+    // Fields
+    // Audio
     this.audioCtx = new AudioContext();
-    this.video = null;
-    this.gainNode= this.audioCtx.createGain();
+    this.source = null;
+    this.videoEl = null;
+    this.input = this.audioCtx.createGain();
+    this.peakings = new Array(10);
     this.nonPitchChangeMode = this.audioCtx.createGain();
     this.pitchChangeMode= this.audioCtx.createGain();
     this.jungle = new Jungle(this.audioCtx);
+    this.output = this.audioCtx.createGain();
+    // parameter
+    this.loop = false;
+    this.loopStart = 0;
+    this.loopEnd = 0;
+    this.pitch = 0;
+    // other
+    this.alreadyLoaded = false;
 
-    this.gainNode.connect(this.pitchChangeMode);
-    this.gainNode.connect(this.nonPitchChangeMode);
-    this.pitchChangeMode.connect(this.jungle.input);
-    this.nonPitchChangeMode.connect(this.audioCtx.destination);
-    this.jungle.output.connect(this.audioCtx.destination);
-    this.gainNode.gain.value = 0.5;
+    connectNode(this);
+
+    // default settings
+    this.input.gain.value = 1;
+    this.output.gain.value = 1;
     this.jungle.setPitchOffset(0, false);
     this.pitchChangeMode.gain.value = 0;
     this.nonPitchChangeMode.gain.value = 1;
 
     assignEvent(this);
+
+    loadVideo(this);
   };
 
-  // add methods
+  // methods
   Content.prototype = {
-    on: function() {
-      var video = document.getElementsByClassName("video-stream")[0];
-      if (video.length === 0) {
-        console.log('not found video');
-      } else {
-        this.video = this.audioCtx.createMediaElementSource(video);
-        this.video.connect(this.gainNode);
-      }
-    },
-    off: function() {
-      this.video = null;
-    },
     play: function() {
-      this.video.mediaElement.play();
+      this.videoEl.play();
     },
     pause: function() {
-      this.video.mediaElement.pause();
+      this.videoEl.pause();
     },
     back: function(seconds) {
-      this.video.mediaElement.currentTime = this.video.mediaElement.currentTime - seconds;
+      this.videoEl.currentTime = this.videoEl.currentTime - seconds;
+    },
+    changeTime: function(seconds) {
+      this.videoEl.currentTime = seconds;
     },
     changeVolume: function(volume) {
-      this.gainNode.gain.value = volume;
+      this.videoEl.volume = volume;
     },
     changeSpeed: function(speed) {
-      this.video.mediaElement.playbackRate = speed;
+      this.videoEl.playbackRate = speed;
     },
     changePitch: function(pitch) {
+      this.pitch = pitch;
       if (pitch === 0){
         this.nonPitchChangeMode.gain.value = 1;
         this.pitchChangeMode.gain.value = 0;
@@ -61,74 +70,191 @@
         this.pitchChangeMode.gain.value = 1;
       }
       this.jungle.setPitchOffset(pitchConvert(pitch), false);
+    },
+    enableLoop: function(isEnabled) {
+      this.loop = isEnabled;
+    },
+    setLoopStart: function(seconds) {
+      this.loopStart = seconds;
+    },
+    setLoopEnd: function(seconds) {
+      this.loopEnd = seconds;
+    },
+    changeEq: function(zoneIdx, gain) {
+      this.peakings[zoneIdx].gain.value = gain;
+    },
+    resetEq: function() {
+      this.peakings.forEach(function (peaking) {
+        peaking.gain.value = 0;
+      });
     }
   };
-
-  // functions
-  function assignEvent(self) {
-    chrome.runtime.onMessage.addListener(function(message) {
-      switch (message.type) {
-        case 'on': {
-          self.on();
-          break;
-        }
-        case 'off': {
-          self.off();
-          break;
-        }
-        case 'play': {
-          self.play();
-          break;
-        }
-        case 'pause': {
-          self.pause();
-          break;
-        }
-        case 'changeTime': {
-          self.changeTime(message.seconds);
-          break;
-        }
-        case 'enableLoop': {
-          self.enableLoop();
-          break;
-        }
-        case 'setLoopStart': {
-          self.setLoopStart(message.seconds);
-          break;
-        }
-        case 'setLoopEnd': {
-          self.setLoopEnd(message.seconds);
-          break;
-        }
-        case 'back': {
-          self.back(message.seconds);
-          break;
-        }
-        case 'changeVolume': {
-          self.changeVolume(message.volume);
-          break;
-        }
-        case 'changeSpeed': {
-          self.changeSpeed(message.speed);
-          break;
-        }
-        case 'changePitch': {
-          self.changePitch(message.pitch);
-          break;
-        }
-      }
-    });
-  }
-  function pitchConvert(originPitchNum) {
-    if (originPitchNum > 0) {
-      return (Math.pow(Math.pow(2, 1/ 12), originPitchNum) - 1) * 2;
-    } else if (originPitchNum < 0) {
-      return - Math.pow(Math.pow(2, 1/ 12), originPitchNum);
-    } else {
-      return 0;
-    }
-  }
-
   // create instance
   new Content();
 })();
+
+
+/**
+ * functions
+ */
+
+function connectNode(that) {
+  eqSet(that);
+  that.input.connect(that.peakings[0]);
+  that.peakings[9].connect(that.pitchChangeMode);
+  that.peakings[9].connect(that.nonPitchChangeMode);
+  that.pitchChangeMode.connect(that.jungle.input);
+  that.nonPitchChangeMode.connect(that.output);
+  that.jungle.output.connect(that.output);
+  that.output.connect(that.audioCtx.destination);
+}
+function eqSet(that) {
+  var frequency = 31.25;
+  for (var i = 0; i < 10; i++) {
+    var peaking = that.audioCtx.createBiquadFilter();
+    if (i !== 0) {
+      frequency *= 2;
+    }
+    peaking.type = (typeof peaking.type === 'string') ? 'peaking' : 5;
+    peaking.frequency.value = frequency;
+    peaking.Q.value = 2;
+    peaking.gain.value = 0;
+    that.peakings[i] = peaking;
+  }
+  that.peakings.forEach(function(peaking, index, ps) {
+    if (index < 9) {
+      peaking.connect(ps[index + 1]);
+    }
+  });
+}
+
+function loadVideo(that) {
+  var video = document.getElementsByTagName('video')[0];
+  if (video === undefined) {
+    console.log('not found video');
+  } else {
+    console.log(video);
+    that.alreadyLoaded = true;
+    that.source = that.audioCtx.createMediaElementSource(video);
+    that.videoEl = that.source.mediaElement;
+    that.source.connect(that.input);
+    console.log(that.source);
+    video.addEventListener('loadeddata', function() {
+      console.log('loadeddata');
+      that.loopEnd = video.duration;
+      // 動画が変化したときなどのイベント
+    });
+    // video.addEventListener('timeupdate', function() {
+    //   // update current time
+    //   chrome.runtime.sendMessage({type: 'every_seconds', currentTime: that.source.mediaElement.currentTime, duration: that.source.mediaElement.duration}, function(response) {
+    //   });
+    //   // Loop setting
+    //   if (that.loop && that.loopEnd <= that.source.mediaElement.currentTime) {
+    //     that.source.mediaElement.currentTime = that.loopStart;
+    //   }
+    // });
+  }
+}
+
+function assignEvent(that) {
+  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    switch (message.type) {
+      case 'init': {
+        sendResponse({
+          isValid: true,
+          currentTime: that.videoEl.currentTime,
+          duration: that.videoEl.duration,
+          loop: that.loop,
+          loopStart: that.loopStart,
+          loopEnd: that.loopEnd,
+          isPaused: that.videoEl.paused,
+          volume: that.videoEl.volume,
+          speed: that.videoEl.playbackRate,
+          pitch: that.pitch,
+          eqVals: that.peakings.map(function(peaking) {
+            return peaking.gain.value;
+          })
+        });
+        that.videoEl.addEventListener('timeupdate', function() {
+          // update current time
+          chrome.runtime.sendMessage({type: 'every_seconds', tabId: message.tabId, currentTime: that.source.mediaElement.currentTime, duration: that.source.mediaElement.duration}, function(response) {
+          });
+          // Loop setting
+          if (that.loop && that.loopEnd <= that.source.mediaElement.currentTime) {
+            that.source.mediaElement.currentTime = that.loopStart;
+            if (that.videoEl.paused) {
+              that.videoEl.play();
+            }
+          }
+        });
+        return true;
+      }
+      case 'reloadVideo': {
+        if (!that.alreadyLoaded) {
+          setTimeout(function () {
+            loadVideo(that);
+          }, 500);
+        }
+        break;
+      }
+      case 'play': {
+        that.play();
+        break;
+      }
+      case 'pause': {
+        that.pause();
+        break;
+      }
+      case 'back': {
+        that.back(message.seconds);
+        break;
+      }
+      case 'changeTime': {
+        that.changeTime(message.seconds);
+        break;
+      }
+      case 'changeVolume': {
+        that.changeVolume(message.volume);
+        break;
+      }
+      case 'changeSpeed': {
+        that.changeSpeed(message.speed);
+        break;
+      }
+      case 'changePitch': {
+        that.changePitch(message.pitch);
+        break;
+      }
+      case 'enableLoop': {
+        that.enableLoop(message.isEnabled);
+        break;
+      }
+      case 'setLoopStart': {
+        that.setLoopStart(message.seconds);
+        break;
+      }
+      case 'setLoopEnd': {
+        that.setLoopEnd(message.seconds);
+        break;
+      }
+      case 'changeEq': {
+        that.changeEq(message.zoneIdx, message.gain);
+        break;
+      }
+      case 'resetEq': {
+        that.resetEq();
+        break;
+      }
+    }
+  });
+}
+
+function pitchConvert(originPitchNum) {
+  if (originPitchNum > 0) {
+    return (Math.pow(Math.pow(2, 1/ 12), originPitchNum) - 1) * 2;
+  } else if (originPitchNum < 0) {
+    return -1 + (Math.pow(Math.pow(2, 1/ 12), (12 + originPitchNum)) - 1);
+  } else {
+    return 0;
+  }
+}
--- scripts/popup.js
+++ scripts/popup.js
@@ -3,92 +3,337 @@
 (function () {
   // constructor
   var Popup = function() {
-    var self = this;
-    this.isOn = false;
-    this.isPlay = true;
-    this.bg = null;
-
-    chrome.runtime.getBackgroundPage(function(backgroundPage) {
-      self.bg = backgroundPage.bg;
-      $('#volume-range').val(self.bg.volume);
+    var that = this;
+    // fields
+    this.seeking = false;
+    this.isValid = false;
+    this.currentTime = 0;
+    this.currentTime = 0;
+    this.duration = 0;
+    this.loop = false;
+    this.loopStart = 0;
+    this.loopEnd = 0;
+    this.isPaused = true;
+    this.volume = 50;
+    this.speed = 100;
+    this.pitch = 0;
+    this.eqVals = new Array(10);
+    this.eqVals.forEach(function(val) {
+      val = 0;
+    });
+    this.slider = document.getElementById('slider');
+    noUiSlider.create(this.slider, {
+      start: [0, 100],
+      connect: true,
+      behaviour: 'none',
+      range: {
+        'min': 0,
+        'max': 100
+      }
     });
   };
+  // methods
+  // Popup.prototype = {
+  // };
+
   // create instance
   var popup = new Popup();
 
+  // send initialization message to content script
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'init', tabId: tabs[0].id}, function(response) {
+      console.log(response);
+      if (response.isValid) {
+        popup.currentTime = response.currentTime;
+        $('#seek-bar-range').val(response.currentTime);
+        $('#current-time').text(convertSeconds(response.currentTime));
+        popup.duration = response.duration;
+        $('#seek-bar-range').attr('max', response.duration);
+        $('#duration').text(convertSeconds(response.duration));
+        popup.slider.noUiSlider.updateOptions({
+          range: {
+            'min': 0,
+            'max': response.duration
+          }
+        });
+        popup.loop = response.loop;
+        $('#loop-switch').attr('checked', response.loop);
+        popup.loopStart = response.loopStart;
+        $("#loop-start-num").text(convertSeconds(response.loopStart));
+        popup.loopEnd = response.loopEnd;
+        $("#loop-end-num").text(convertSeconds(response.loopEnd));
+        popup.slider.noUiSlider.set([response.loopStart, response.loopEnd]);
+        popup.isPaused = response.isPaused;
+        if (response.isPaused) {
+          $('#play-btn-svg').css('display', 'inline');
+          $('#pause-btn-svg').css('display', 'none');
+        } else {
+          $('#play-btn-svg').css('display', 'none');
+          $('#pause-btn-svg').css('display', 'inline');
+        }
+        popup.volume = response.volume * 100;
+        $('#volume-range').val(response.volume * 100);
+        $("#volume-num").text(Math.floor(response.volume * 100));
+        popup.speed = response.speed * 100;
+        $('#speed-range').val(response.speed * 100);
+        $("#speed-num").text('x' + response.speed);
+        popup.pitch = response.pitch;
+        $('#pitch-range').val(response.pitch);
+        $("#pitch-num").text(response.pitch);
+        response.eqVals.forEach(function (val, idx) {
+          popup.eqVals[idx] = val;
+          $('#eq' + (idx + 1)).val(val);
+          $('#eq' + (idx + 1) + '-value').text(val + 'db');
+        });
+      }
+    });
+  });
+
   // Event Listeners
-  $('#on-off-slider').on('click', function () {
-    var input = $('#on-off-input');
-    input.prop('checked', !input.prop('checked'));
-    if (popup.isOn) {
+  $('#loop-slider').on('click', function() {
+    if(popup.loop) {
+      $('#loop-switch').attr('checked', false);
+      popup.loop  = false;
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'off'});
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'enableLoop', isEnabled: false});
       });
     } else {
+      $('#loop-switch').attr('checked', true);
+      popup.loop  = true;
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'on'});
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'enableLoop', isEnabled: true});
       });
     }
-    popup.isOn = !popup.isOn;
+  });
+  $('#seek-bar-range').on('change', function() {
+    var val = Number($(this).val());
+    popup.seeking = false;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeTime', seconds: val});
+    });
+  });
+  $('#seek-bar-range').on('input', function() {
+    var val = Number($(this).val());
+    popup.seeking = true;
+    $('#current-time').text(convertSeconds(val));
   });
   $('#play-btn').on('click', function () {
-    if (popup.isPlay) {
-      $('#play-btn-svg').css('display', 'inline');
-      $('#pause-btn-svg').css('display', 'none');
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {type: 'pause'});
-      });
-    } else {
+    if (popup.isPaused) {
       $('#play-btn-svg').css('display', 'none');
       $('#pause-btn-svg').css('display', 'inline');
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {type: 'play'});
       });
+    } else {
+      $('#play-btn-svg').css('display', 'inline');
+      $('#pause-btn-svg').css('display', 'none');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'pause'});
+      });
     }
-    popup.isPlay = !popup.isPlay;
+    popup.isPaused = !popup.isPaused;
   });
   $('#replay-five-btn').on('click', function () {
-    console.log('replay-five-btn-clicked');
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {type: 'back', seconds: 5});
     });
   });
   $('#replay-ten-btn').on('click', function () {
-    console.log('replay-ten-btn-clicked');
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {type: 'back', seconds: 10});
     });
   });
   $('#replay-fifteen-btn').on('click', function () {
-    console.log('replay-fifteen-btn-clicked');
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {type: 'back', seconds: 30});
     });
   });
   $('#volume-range').on('input', function () {
-    var val = $(this).val();
-    var volume = Number(val) / 100;
-    $("#volumen-num").text(val);
+    var val = Number($(this).val());
+    $("#volume-num").text(val);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeVolume', volume: volume});
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeVolume', volume: val / 100});
     });
-    console.log(popup.bg);
-    popup.bg.volume = val;
   });
   $('#speed-range').on('input', function () {
-    var val = $(this).val();
-    var speed = Number(val) / 100;
-    $("#speed-num").text('x' + speed);
+    var val = Number($(this).val());
+    $("#speed-num").text('x' + val / 100);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeSpeed', speed: speed});
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeSpeed', speed: val / 100});
     });
   });
   $('#pitch-range').on('input', function () {
-    var val = $(this).val();
-    var pitch = Number(val);
+    var val = Number($(this).val());
     $("#pitch-num").text(val);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {type: 'changePitch', pitch: pitch});
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changePitch', pitch: val});
     });
   });
+  $('#volume-reset-btn').on('click', function () {
+    $("#volume-num").text(50);
+    $('#volume-range').val(50);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeVolume', volume: 50 / 100});
+    });
+  });
+  $('#speed-reset-btn').on('click', function () {
+    $("#speed-num").text('x' + 100 / 100);
+    $('#speed-range').val(100);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeSpeed', speed: 100 / 100});
+    });
+  });
+  $('#pitch-reset-btn').on('click', function () {
+    $("#pitch-num").text(0);
+    $('#pitch-range').val(0);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changePitch', pitch: 0});
+    });
+  });
+  $('#eq1').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq1-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 0, gain: val});
+    });
+  });
+  $('#eq2').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq2-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 1, gain: val});
+    });
+  });
+  $('#eq3').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq3-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 2, gain: val});
+    });
+  });
+  $('#eq4').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq4-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 3, gain: val});
+    });
+  });
+  $('#eq5').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq5-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 4, gain: val});
+    });
+  });
+  $('#eq6').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq6-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 5, gain: val});
+    });
+  });
+  $('#eq7').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq7-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 6, gain: val});
+    });
+  });
+  $('#eq8').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq8-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 7, gain: val});
+    });
+  });
+  $('#eq9').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq9-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 8, gain: val});
+    });
+  });
+  $('#eq10').on('input', function () {
+    var val = Number($(this).val());
+    $('#eq10-value').text(val + 'db');
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'changeEq', zoneIdx: 9, gain: val});
+    });
+  });
+  $('#eq-reset-btn').on('click', function () {
+    for (var i = 0; i < 10; i++) {
+      $('#eq' + (i + 1)).val(0);
+      $('#eq' + (i + 1) + '-value').text(0 + 'db');
+    }
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'resetEq'});
+    });
+  });
+  popup.slider.noUiSlider.on('end', function(values, handle){
+    if (handle === 0) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'setLoopStart', seconds: Number(values[0])});
+      });
+    } else if (handle === 1) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: 'setLoopEnd', seconds: Number(values[1])});
+      });
+    }
+  });
+  popup.slider.noUiSlider.on('update', function(values, handle){
+    if (handle === 0) {
+      $("#loop-start-num").text(convertSeconds(values[0]));
+    } else if (handle === 1) {
+      $("#loop-end-num").text(convertSeconds(values[1]));
+    }
+  });
+
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        if(tabs[0].id === request.tabId) {
+          if (request.type === 'every_seconds') {
+            console.log(request);
+            if (!popup.seeking) {
+              $('#seek-bar-range').val(request.currentTime);
+              $('#current-time').text(convertSeconds(request.currentTime));
+              if (request.duration !== popup.duration) {
+                popup.duration = request.duration;
+                $('#seek-bar-range').attr('max', request.duration);
+                $('#duration').text(convertSeconds(request.duration));
+                popup.slider.noUiSlider.updateOptions({
+                  range: {
+                    'min': 0,
+                    'max': request.duration
+                  }
+                });
+                popup.slider.noUiSlider.set([0, request.duration]);
+                popup.loopStart = 0;
+                popup.loopStart = request.duration;
+                $('#loop-start-num').text(convertSeconds(0));
+                $('#loop-end-num').text(convertSeconds(request.duration));
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                  chrome.tabs.sendMessage(tabs[0].id, {type: 'setLoopStart', seconds: popup.loopStart});
+                });
+                chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                  chrome.tabs.sendMessage(tabs[0].id, {type: 'setLoopEnd', seconds: popup.loopEnd});
+                });
+              }
+            }
+          }
+        }
+      });
+    }
+  );
+
 })();
+
+
+/* functions */
+
+function convertSeconds(rawSeconds) {
+  var minutes = Math.floor(rawSeconds / 60);
+  var seconds = Math.floor(rawSeconds % 60);
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  return minutes + ':' + seconds;
+}
